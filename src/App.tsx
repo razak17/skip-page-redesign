298ddaf428e53343a334011f6e1d5d95a7d556cb
Abdul Razak <moabdulrazak17@gmail.com>
2025-06-08 17:08:54 +0000

    chore: initial react app

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-red-500 text-2xl">Welcome to My App</h1>
+      <p>This is a simple React application.</p>
+    </div>
+  );
+};
+
+export default App;
